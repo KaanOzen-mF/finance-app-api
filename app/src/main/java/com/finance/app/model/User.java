7199b10e8391554eaf9fb591ef47a4f7326e26bf
KaanOzen-mF <kaanozn98@gmail.com>
2025-08-18 12:21:01 +0300

    add User entity for user management

---
+++ app/src/main/java/com/finance/app/model/User.java
@@ -0,0 +1,34 @@
+package com.finance.app.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
